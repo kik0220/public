90012db825c517c87f5ac45f5972d452b558d56e
kik0220 <kik0220@gmail.com>
2013-06-17 17:49:56 +0900

    install automatic

--- squid-inst.sh
+++ squid-inst.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 curl -L https://raw.github.com/kik0220/public/master/squid.conf > squid.conf
-nano squid.conf
 sudo yum install squid -y
 sudo mv /etc/squid/squid.conf /etc/squid/squid.conf_org
 sudo mv squid.conf /etc/squid/

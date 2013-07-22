21f782d7bd732b8cea6ebfc1f734405d82b244c6
kik0220 <kik0220@gmail.com>
2013-07-22 17:07:16 +0900

    addThis Feedly

--- addThis-Feedly.js
+++ addThis-Feedly.js
@@ -1,40 +1,36 @@
-(function(){
-  function main() {
-    addThisButton();
-    document.addEventListener("DOMSubtreeModified",function(event) {
-      setTimeout(addThisButton(), 3000);
-    });
-  }
-  function addThisButton() {
-    var inlineFrames = document.getElementsByClassName('u100Frame');
-    if( inlineFrames.length < 1){
-      inlineFrames = document.getElementsByClassName('inlineFrame');
+javascript:(function(){
+  if(document.location.href.indexOf('//cloud.feedly.com/')<0)return;
+  var head_script=document.createElement('script');
+  head_script.type='text/javascript';
+  head_script.src='//s7.addthis.com/js/300/addthis_widget.js#domready=1&async=1';
+  document.head.appendChild(head_script);
+  add_addthis();
+  document.addEventListener("DOMSubtreeModified",function(e){
+    setTimeout(add_addthis(),3000);
+  });
+  function add_addthis(){
+    var entries=document.getElementsByClassName('u100Frame');
+    if( entries.length<1){
+      entries=document.getElementsByClassName('inlineFrame');
     }
-    for(var i = 0; i < inlineFrames.length; i++){
-      var inlineFrame=inlineFrames[i];
-      if( inlineFrame.getElementsByClassName('addThisUnOfficial').length > 0){
+    for(var i=0;i<entries.length;i++){
+      var entry=entries[i];
+      var entry_title=entry.getElementsByClassName('entryTitle');
+      if(entry_title.length<1){
         continue;
       }
-      var inlineTitle = inlineFrame.getElementsByClassName('entryTitle');
-      if( inlineTitle.length < 1) {
+      if(entry.getElementsByClassName('addThisUnOfficial').length>0){
         continue;
       }
-      var addThisPosition = inlineFrame.getElementsByClassName('abZone')[0].parentNode;
-      var addThisDiv = document.createElement('div');
-      addThisDiv.id = inlineFrame.id+'_addThisUnOfficial';
-      addThisDiv.className = 'addthis_toolbox addthis_default_style addThisUnOfficial';
-      addThisPosition.appendChild( addThisDiv);
-      var addThisLink = document.createElement('a');
-      addThisLink.className = 'addthis_button_compact';
-      addThisDiv.appendChild( addThisLink);
-      var addThisScript = document.createElement('script');
-      addThisScript.type = 'text/javascript';
-      addThisScript.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51db39db77a83636&domready=1';
-      addThisPosition.appendChild(addThisScript);
-      try{
-        addthis.toolbox(document.getElementById(addThisDiv.id), {},{ url : inlineTitle[0].href, title : inlineTitle[0].innerText});
-      } catch(e) {}
+      var add_div_position=entry.getElementsByClassName('abZone')[0].parentNode;
+      var add_div=document.createElement('div');
+      add_div.className='addthis_toolbox addthis_default_style addThisUnOfficial';
+      add_div.style.padding.top='2px';
+      add_div_position.appendChild(add_div);
+      add_div.innerHTML='<a class="addthis_button_compact" addthis:url='+entry_title[0].href+' addthis:title='+entry_title[0].innerText+' />';
+      var add_div_script=document.createElement('script');
+      add_div_position.appendChild(add_div_script);
+      add_div_script.innerText='addthis.toolbox(".addThisUnOfficial");';
     }
   }
-  main();
 })();

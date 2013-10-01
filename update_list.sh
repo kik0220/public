3b79c3351812405c359032305ef6571f8ad9b903
kik0220 <kik0220@gmail.com>
2013-10-01 20:00:42 +0900

    yads.yahoo.co.jpを追加

--- update_list.sh
+++ update_list.sh
@@ -2,6 +2,6 @@ rm -f easylist.txt
 echo "[Adblock Plus 1.1]" > easy_modify_list.txt
 echo "! Title: Easy modify List" >> easy_modify_list.txt
 echo "! Update: "`date -I` >> easy_modify_list.txt
-curl https://easylist-downloads.adblockplus.org/easylist.txt|sed -e "/^@@.*/d"|sed -e "/^\[.*/d"|sed -e "/! Checksum:.*/d"|sed -e "/! Title:.*/d"|sed -e "/! Version:.*/d" >> easy_modify_list.txt
+curl https://easylist-downloads.adblockplus.org/easylist.txt|sed -e "/^@@.*/d"|sed -e "/\[/d"|sed -e "/^! [CVTLHPio].* /d" >> easy_modify_list.txt
 cat easy_modify_list.txt|sed -e "s/! Title: .*/! Title: Easy modify List \+ Japanese Filter/" > easylist+fanboy-japanese+myfilter.txt
 cat fanboy-japanese+myfilter.txt >> easylist+fanboy-japanese+myfilter.txt
